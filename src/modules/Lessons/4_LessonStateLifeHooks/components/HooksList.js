133587b57516ad371a94865141e97f1b0ae644dc
ifzal <ifzal.ahmed@arbisoft.com>
2018-10-03 10:36:15 +0500

    Assignment#1: Class based child component.

--- src/modules/Lessons/4_LessonStateLifeHooks/components/HooksList.js
+++ src/modules/Lessons/4_LessonStateLifeHooks/components/HooksList.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+class HooksList extends Component {
+  constructor(props) {
+    super(props);
 
-const HooksList = ({countries, fontColor}) => {
-  console.log("..... countries in HooksList .......", countries);
-  console.log("..... fontColor .......", fontColor);
-  const isLoading = !countries;
+    this.state = {
+      fontColor: 'red',
+      countries: [],
+      isLoading: true,
+    }
+  }
 
-  return isLoading ? <h3>Loading......</h3> : (
-      <ul className="countries-list">
-        {
-          countries.map(country => <li key={country.value} style={{color:fontColor}}>{country.label}</li>)
-        }
-      </ul>
-  );
+  componentWillReceiveProps(nextProps) {
+    this.setState({fontColor:nextProps.fontColor});
+    if (this.state.countries !== nextProps.countries) {
+      this.setState({countries: nextProps.countries, isLoading: !nextProps.countries});
+    }
 
-};
+  }
+
+  render() {
+    const {countries, fontColor, isLoading} = this.state;
+    return isLoading ? <h3>Loading......</h3> : (
+        <ul className="countries-list">
+          {
+            countries.map(country => <li key={country.value} style={{color: fontColor}}>{country.label}</li>)
+          }
+        </ul>
+    );
+  }
+
+}
 
 HooksList.propTypes = {
   fontColor: PropTypes.string,
