179318df4e93d65cedbb888e0c246a509fb58271
ifzal <ifzal.ahmed@arbisoft.com>
2018-10-24 22:27:13 +0500

    Assignment#7: A little refactoring so routes can be rendered dynamically.

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import routes from '../../routes';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Sidebar from "./components/Sidebar/Sidebar";
 
+import LessonJSX from "../Lessons/1_LessonJSX/LessonJSX";
+import LessonElements from "../Lessons/2_LessonElements/LessonElements";
+import LessonCompProps from "../Lessons/3_LessonCompProps/LessonCompProps";
+import LessonStateLifeHooks from "../Lessons/4_LessonStateLifeHooks/LessonStateLifeHooks";
+import LessonConditionalRenders from "../Lessons/5_LessonConditionalRenders/LessonConditionalRenders";
+import LessonStateLiftup from "../Lessons/6_LessonStateLiftup/LessonStateLiftup";
+import LessonContainerPresentational from "../Lessons/7_LessonContainerPresentational/LessonContainerPresentational";
+import LessonUserData from "../Lessons/8_LessonUserData/LessonUserData";
+import LessonAPIIntegration from "../Lessons/9_LessonAPIIntegration/LessonAPIIntegration";
+
 import './App.css';
 import '../Lessons/Lessons.css';
+import WorkshopRoutes from "../../routes";
+
+
+const LESSONS = {
+  'jsx': {title: 'Introduction to JSX', link: '/lesson/jsx', lesson: LessonJSX},
+  'elements': {title: 'React Elements', link: '/lesson/elements', lesson: LessonElements},
+  'components-props': {title: 'Components and Props', link: '/lesson/components-props', lesson: LessonCompProps},
+  'state-lifecycle-hooks': {title: 'State & Lifecycle Hooks', link: '/lesson/state-lifecycle-hooks', lesson: LessonStateLifeHooks},
+  'conditional-rendering': {title: 'Conditional Rendering', link: '/lesson/conditional-rendering', lesson: LessonConditionalRenders},
+  'state-liftup': {title: 'State Lift-up', link: '/lesson/state-liftup', lesson: LessonStateLiftup},
+  'container-vs-presentational': {title: 'Container vs. Presentational', link: '/lesson/container-vs-presentational', lesson: LessonContainerPresentational},
+  'handling-user-data': {title: 'Handling User Data', link: '/lesson/handling-user-data', lesson: LessonUserData},
+  'api-integration': {title: 'API Integration', link: '/lesson/api-integration', lesson: LessonAPIIntegration},
+};
+
+const LessonLoader = (props) => {
+  const lessonSlug = props.match.params.slug;
+  const Lesson = LESSONS[lessonSlug].lesson;
+    return (
+        <Lesson title={LESSONS[lessonSlug].title}/>
+    )
+};
+
 
 class App extends Component {
   render() {
@@ -14,9 +46,9 @@ class App extends Component {
       <div className="App">
         <Header/>
         <div className="lessons">
-          <Sidebar/>
+          <Sidebar navLinks={Object.values(LESSONS)}/>
           <div className="lesson-body">
-            {routes}
+            <WorkshopRoutes lessonLoader={LessonLoader}/>
           </div>
         </div>
 
--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
-import LessonJSX from './modules/Lessons/1_LessonJSX/LessonJSX';
-import LessonElements from './modules/Lessons/2_LessonElements/LessonElements';
-import LessonCompProps from './modules/Lessons/3_LessonCompProps/LessonCompProps';
-import LessonStateLifeHooks from './modules/Lessons/4_LessonStateLifeHooks/LessonStateLifeHooks';
-import LessonConditionalRenders from './modules/Lessons/5_LessonConditionalRenders/LessonConditionalRenders';
-import LessonStateLiftup from './modules/Lessons/6_LessonStateLiftup/LessonStateLiftup';
-import LessonContainerPresentational from './modules/Lessons/7_LessonContainerPresentational/LessonContainerPresentational';
-import LessonUserData from './modules/Lessons/8_LessonUserData/LessonUserData';
-import LessonAPIIntegration from './modules/Lessons/9_LessonAPIIntegration/LessonAPIIntegration';
 import Dashboard from "./modules/Lessons/Dashboard/Dashboard";
 import PageNotFound from "./modules/App/components/ErrorPage/PageNotFound";
 
 
-export default (
-    <Switch>
-      <Route path="/" exact render={(props) => <Dashboard {...props}/>} />
-      <Route path="/lesson/jsx" render={(props) => <LessonJSX {...props}/>} />
-      <Route path="/lesson/elements" component={LessonElements} />
-      <Route path="/lesson/components-props" component={LessonCompProps} />
-      <Route path="/lesson/state-lifecycle-hooks" component={LessonStateLifeHooks} />
-      <Route path="/lesson/conditional-rendering" component={LessonConditionalRenders} />
-      <Route path="/lesson/state-liftup" component={LessonStateLiftup} />
-      <Route path="/lesson/container-vs-presentational" component={LessonContainerPresentational} />
-      <Route path="/lesson/handling-user-data" component={LessonUserData} />
-      <Route path="/lesson/api-integration" component={LessonAPIIntegration} />
 
-      <Route component={PageNotFound}/>
-    </Switch>
-);
+const WorkshopRoutes = ({lessonLoader}) => {
+  return (
+      <Switch>
+        <Route path="/" exact render={(props) => <Dashboard {...props}/>}/>
+
+        <Route path="/lesson/:slug" component={lessonLoader}/>
+
+        <Route component={PageNotFound}/>
+      </Switch>
+  )
+};
+
+export default WorkshopRoutes;
